814e88b7bb76a59a827c5f15047d90d0fbe2447a
atomlin <atomlikn@gmail.com>
2024-07-05 02:28:16 +0300

    removed callback treatment

--- source/main.cpp
+++ source/main.cpp
@@ -2,16 +2,8 @@
 
 proxy server = { 1337 };
 
-callback udp_callback;
-
 int main()
 {
-    udp_callback.install([](std::string source_ip, std::string dest_ip, unsigned short source_port, unsigned short dest_port, unsigned char* data, unsigned int length) -> bool {
-        printf("[%s -> %s | %d -> %d] length: %d\n\n", source_ip.c_str(), dest_ip.c_str(), source_port, dest_port, length);
-        return false;
-    });
-    server.callback_enable(proxys::callback_udp, &udp_callback);
-    
     server.set_auth_data("admin", "neadmin");
     while(1) Sleep(25);
 }
