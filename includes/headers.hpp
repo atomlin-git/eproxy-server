e42997dfc92e4a27e5e2072da5982ad256d13f4e
atomlin-git <atomlikn@gmail.com>
2024-07-12 20:08:12 +0300

    sideways movements to crossplatform

--- includes/headers.hpp
+++ includes/headers.hpp
@@ -1,4 +1,6 @@
 #pragma once
 
+#include <chrono>
+
 #include "utils.hpp"
 #include "proxy.hpp"
--- source/main.cpp
+++ source/main.cpp
@@ -5,5 +5,6 @@ proxy server = { 1337 };
 int main()
 {
     server.set_auth_data("admin", "neadmin");
-    while(1) Sleep(25);
+
+    while(1) std::this_thread::sleep_for(std::chrono::milliseconds(45));
 }
