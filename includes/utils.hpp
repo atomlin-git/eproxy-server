594629dd96189074f3ab864725f48f05ffc75263
atomlin-git <atomlikn@gmail.com>
2024-07-12 19:50:01 +0300

    new method to utils

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -2,12 +2,13 @@
 
 #include <string>
 #include <format>
+#include <vector>
+#include <sstream>
 
 template<typename return_type>
 class callback
 {
     void* dest;
-
     public:
         template <class T>
             bool install(T idest) {
@@ -25,7 +26,19 @@ class callback
 class utils
 {
     public:
-        std::string dip_to_strip(unsigned int decimal_ip) { 
+        static std::string dip_to_strip(unsigned int decimal_ip) { 
             return std::format("{}.{}.{}.{}", (unsigned char)(decimal_ip & 0x000000ff), (unsigned char)((decimal_ip & 0x0000ff00) >> 8), (unsigned char)((decimal_ip & 0x00ff0000) >> 16), (unsigned char)((decimal_ip & 0xff000000) >> 24));
+        };
+
+        static unsigned int strip_to_dip(const std::string string_ip) {
+            std::vector<std::string> parts;
+            std::stringstream ss(string_ip);
+            unsigned int result = 0;
+            std::string part;
+            
+            while (std::getline(ss, part, '.')) parts.push_back(part);
+            for (int i = 0; i < 4; i++) result |= std::stoi(parts[i]) << (8 * i);
+            
+            return result;
         };
 };
