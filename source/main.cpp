e7b2a0b59ed014570ef57a242d7cc845bb2ee064
atomlin-git <109459940+atomlin-git@users.noreply.github.com>
2024-09-23 20:45:46 +0300

    remove default auth data

--- source/main.cpp
+++ source/main.cpp
@@ -4,7 +4,6 @@
     proxy proxy_server = { 1337 };
 
     int main() {
-        proxy_server.set_auth_data("admin", "neadmin");
         while(1) std::this_thread::sleep_for(std::chrono::milliseconds(45));
     };
 #endif
