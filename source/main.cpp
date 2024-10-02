3fa2bb295c16c9f6382324ec93aa42192b9c527b
atomlin-git <atomlikn@gmail.com>
2024-10-02 17:47:34 +0300

    one header library solution

--- source/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "headers.hpp"
-
-#ifndef EPS_LIBRARY
-    proxy proxy_server = { 1337 };
-
-    int main() {
-        while(1) std::this_thread::sleep_for(std::chrono::milliseconds(45));
-    };
-#endif
