fe4760d72d9c41887e2e090b49611087071b51c8
atomlin-git <atomlikn@gmail.com>
2024-12-16 01:25:01 +0300

    more right sol

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,8 @@
 #include <chrono>
 #include "proxy.hpp"
 
-#ifndef EPS_LIBRARY
-    proxy proxy_server = { 1337 };
+proxy proxy_server = { 1337 };
 
-    int main() {
-        while(1) std::this_thread::sleep_for(std::chrono::milliseconds(45));
-    };
-#endif
+int main() {
+    while(1) std::this_thread::sleep_for(std::chrono::milliseconds(45));
+};
